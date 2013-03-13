5019b709f76be19a74c973f2464274e2d618218d
carltoncolter_cp <25727192+carltoncolter@users.noreply.github.com>
2013-03-13 13:19:16 +0000

    Corrected for Update to use Pre and multiple sources (for fullname support)

--- SoundsLikePlugin/SoundsLikePlugin.cs
+++ SoundsLikePlugin/SoundsLikePlugin.cs
@@ -6,7 +6,7 @@ namespace SoundsLike
     public class SoundsLikePlugin : IPlugin
     {
         public Config Configuration;
-        
+
         public SoundsLikePlugin()
         { }
 
@@ -16,73 +16,128 @@ namespace SoundsLike
             Configuration = Config.Deserialize(configString);
         }
 
+        private string GetString(ITracingService tracer, Entity primary, Entity secondary, string field, string prefix = "")
+        {
+            if (field == null || field.Length < 1)
+            {
+                return string.Empty;
+            }
+            tracer.Trace("Getting field {0}", field);
+            if (primary.Attributes.ContainsKey(field))
+            {
+                return String.Format("{0}{1}", prefix, primary.GetAttributeValue<string>(field));
+            }
+            if (secondary.Attributes.ContainsKey(field))
+            {
+                return String.Format("{0}{1}", prefix, secondary.GetAttributeValue<string>(field));
+            }
+            return string.Empty;
+        }
+
         public void Execute(IServiceProvider serviceProvider)
         {
-            //Extract the tracing service
-            var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            if (tracingService == null)
-                throw new InvalidPluginExecutionException("Failed to retrieve the tracing service.");
-
-            // Obtain the execution context from the service provider.
-            var context = (IPluginExecutionContext) serviceProvider.GetService(typeof(IPluginExecutionContext));
-
-            // Only process Create and Update messages
-            if (context.MessageName != "Create" && context.MessageName != "Update") return;
-
-            // Only process if there is a target entity
-            if (!context.InputParameters.Contains("Target") || 
-               (!(context.InputParameters["Target"] is Entity)))
+            try
             {
-                return;
-            }
+                //Extract the tracing service
+                var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+                if (tracingService == null)
+                    throw new InvalidPluginExecutionException("Failed to retrieve the tracing service.");
 
-            // Get the target entity
-            var entity = (Entity) context.InputParameters["Target"];
+                // Obtain the execution context from the service provider.
+                var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
-            // Codify for each setting
-            foreach (var setting in Configuration.Settings)
-            {
-                // Continue if the Target does not contain the field to encode, no need to re-encode
-                // something that wasn't changed
-                if (!entity.Contains(setting.Source))
+                // Only process Create and Update messages
+                if (context.MessageName != "Create" && context.MessageName != "Update")
                 {
-                    // If DisableUpdate is specified, then don't allow someone to manually 
-                    // update the coded text
-                    if (setting.DisableUpdate && entity.Contains(setting.Target))
-                    {
-                        entity.Attributes.Remove(setting.Target);
-                    }
-                    continue;
+                    return;
                 }
-
-                // Get the text to encode
-                var text = entity.GetAttributeValue<string>(setting.Source);
+                tracingService.Trace("SoundsLikePlugin: Create or Update");
                 
-                // Codify the string using the method specified
-                string codifiedText;
-                switch (setting.Method)
+
+                // Only process if there is a target entity
+                Entity pre = null;
+                if (context.PreEntityImages.ContainsKey("pre"))
                 {
-                    case CodificationMethod.Soundex:
-                        codifiedText = Soundex.Codify(text, setting.MinLength, setting.MaxLength, false);
-                        break;
-                    case CodificationMethod.SoundexOriginal:
-                        codifiedText = Soundex.Codify(text, setting.MinLength, setting.MaxLength, true);
-                        break;
-                    case CodificationMethod.Metaphone:
-                        codifiedText = Metaphone.Codify(text, setting.MinLength, setting.MaxLength);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    pre = context.PreEntityImages["pre"];
                 }
 
-                // Add or update the codified attribute);
-                if (entity.Attributes.ContainsKey(setting.Target))
+                if (!context.InputParameters.Contains("Target") ||
+                   (!(context.InputParameters["Target"] is Entity)))
                 {
-                    entity.Attributes[setting.Target] = codifiedText;
-                } else
-                {
-                    entity.Attributes.Add(setting.Target, codifiedText);
+                    tracingService.Trace("SoundsLikePlugin: No Target");
+                    return;
                 }
+                else
+                {
+                    tracingService.Trace("SoundsLikePlugin: Target Found");
+                }
+
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+
+
+                // Codify for each setting
+                foreach (var setting in Configuration.Settings)
+                {
+                    // Continue if the Target does not contain the field to encode, no need to re-encode
+                    // something that wasn't changed
+                    if (entity.Contains(setting.Target))
+                    {
+                        // If DisableUpdate is specified, then don't allow someone to manually 
+                        // update the coded text
+                        if (setting.DisableUpdate && entity.Contains(setting.Target))
+                        {
+                            entity.Attributes.Remove(setting.Target);
+                        }
+                    }
+
+                    // Get the text to encode
+                    tracingService.Trace("SoundsLikePlugin: Getting Text for Sources");
+                    string text = String.Format("{0}{1}{2}",
+                                    GetString(tracingService, entity, pre, setting.Source),
+                                    GetString(tracingService, entity, pre, setting.Source2),
+                                    GetString(tracingService, entity, pre, setting.Source3));
+                    tracingService.Trace("SoundsLikePlugin: Source Text: {0}", text);
+
+                    // Codify the string using the method specified
+                    string codifiedText;
+
+                    tracingService.Trace("SoundsLikePlugin: Codifying Text: {0}", text);
+                    switch (setting.Method)
+                    {
+                        case CodificationMethod.Soundex:
+                            codifiedText = Soundex.Codify(text, setting.MinLength, setting.MaxLength, false);
+                            break;
+                        case CodificationMethod.SoundexOriginal:
+                            codifiedText = Soundex.Codify(text, setting.MinLength, setting.MaxLength, true);
+                            break;
+                        case CodificationMethod.Metaphone:
+                            codifiedText = Metaphone.Codify(text, setting.MinLength, setting.MaxLength);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    tracingService.Trace("SoundsLikePlugin: Codified To: {0}", codifiedText);
+
+                    // Add or update the codified attribute);
+                    if (entity.Attributes.ContainsKey(setting.Target))
+                    {
+                        tracingService.Trace("SoundsLikePlugin: Setting Target Field: {0}", setting.Target);
+                        entity.Attributes[setting.Target] = codifiedText;
+                    }
+                    else
+                    {
+                        tracingService.Trace("SoundsLikePlugin: Adding Target Field: {0}", setting.Target);
+                        entity.Attributes.Add(setting.Target, codifiedText);
+                    }
+
+                    //throw new Exception("doh");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException("SoundsLikePlugin Failed", ex);
             }
         }
     }
